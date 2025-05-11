89f4e2f130c7251c085552a745db5409a7650712
Antoine Revel <antoine.revel@doctolib.com>
2025-05-11 12:26:33 +0200

    alembic working with first migration up

--- DriverScore_backend-main/driver_score/core/__init__.py
+++ DriverScore_backend-main/driver_score/core/__init__.py
@@ -1,5 +1,5 @@
 from driver_score.common.models import (
     DissolvedRoute, Driver, CollectedDataFile, CurveInventory,
     Run, UploadLog, GpsSample, RoadCharacteristic, Score, ImuSample,
-    SpatialRefSy, t_geography_columns, t_geometry_columns
+    
 )
